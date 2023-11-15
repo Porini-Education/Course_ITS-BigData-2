5e603cd7e9269f563ede54ff356f7f2757ff94a4
matteo-beshara-porini <139470392+matteo-beshara-porini@users.noreply.github.com>
2023-11-15 17:49:42 +0100

    database version

--- Python/AzureFunction/HttpTrigger1/__init__.py
+++ Python/AzureFunction/HttpTrigger1/__init__.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+import pyodbc
 
 import azure.functions as func
 
@@ -8,6 +9,22 @@ def computeAge(birthdate_str):
     today = datetime.now()
     age = today - birthdate
     return age.days // 365.25
+
+def insertRecord(name, birthdate):
+    server_name = 'its-azfunc-01-matbes.database.windows.net'
+    database_name = 'test-azfunction'
+    user_name = 'azfunction'
+    password = 'AlfaBravo2023'
+
+    cnxn = pyodbc.connect(f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server_name};DATABASE={database_name};UID={user_name};PWD={password}")
+    cursor = cnxn.cursor()
+
+    query = f"insert into dbo.matbes values ('{name}', '{birthdate}')"
+    response = cursor.execute(query)
+    cursor.commit()
+
+    return response
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -34,6 +51,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         age = computeAge(birthdate)
     
     if name and birthdate:
+        query_result = insertRecord(name, birthdate)
+        logging.info(query_result)
         return func.HttpResponse(f"Hello, {name}. You are {age} so you can access the hidden data.")
     elif not name:
         return func.HttpResponse(f"You are {age}, but you have to provide your name.")
