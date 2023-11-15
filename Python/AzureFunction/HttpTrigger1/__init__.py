7fcb0418e244aa2d3b82d13883be86def2355f69
matteo-beshara-porini <139470392+matteo-beshara-porini@users.noreply.github.com>
2023-11-15 16:37:39 +0100

    updated version

--- Python/AzureFunction/HttpTrigger1/__init__.py
+++ Python/AzureFunction/HttpTrigger1/__init__.py
@@ -1,7 +1,13 @@
 import logging
+from datetime import datetime
 
 import azure.functions as func
 
+def computeAge(birthdate_str):
+    birthdate = datetime.strptime(birthdate_str, '%Y-%m-%d')
+    today = datetime.now()
+    age = today - birthdate
+    return age.days // 365.25
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -15,10 +21,26 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    birthdate = req.params.get('birthdate')
+    if not birthdate:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            birthdate = req_body.get('birthdate')
+    
+    if birthdate:
+        age = computeAge(birthdate)
+    
+    if name and birthdate:
+        return func.HttpResponse(f"Hello, {name}. You are {age} so you can access the hidden data.")
+    elif not name:
+        return func.HttpResponse(f"You are {age}, but you have to provide your name.")
+    elif not birthdate:
+        return func.HttpResponse(f"Hello, {name}, but you have to provide your birthdate.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully. Pass a name and a birthdate in the query string or in the request body for a personalized response.",
              status_code=200
         )
